80efd0f990e6cc450da3f2e1570f7e9f2829815f
sr1011 <ryo.s.1011.football@gmail.com>
2021-11-21 06:01:48 +0000

    delete

--- db/migrate/20211116100243_subscrptons.rb
+++
@@ -1,5 +0,0 @@
-class Subscrptons < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :subscrptons
-  end
-end
--- db/migrate/20211116100352_subscriptons.rb
+++
@@ -1,5 +0,0 @@
-class Subscriptons < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :subscriptons
-  end
-end
