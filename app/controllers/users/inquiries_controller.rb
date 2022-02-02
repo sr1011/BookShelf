935f4b21c181d3fe2f97cec0576a0147a8d29d64
sr1011 <ryo.s.1011.football@gmail.com>
2022-02-02 04:22:29 +0000

    修正

--- app/controllers/users/inquiries_controller.rb
+++ app/controllers/users/inquiries_controller.rb
@@ -1,25 +1,3 @@
 class Users::InquiriesController < ApplicationController
-  
-  
-  def create
-    @inquire = current_user.inquire.new(inquire_params)
-    if @inquire.save
-     redirect_to users_inquiries_path
-    else
-      render :new
-    end
-  end
 
-  def index
-    @inquire = current_user.inquire.all
-  end
-  
-  def destroy
-  end
-  
-  private
-  def inquire_params
-    params.require(:inquire).permit(:title, :massage)
-  end
-  
 end
